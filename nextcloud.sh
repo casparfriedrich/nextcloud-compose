51715ce1ae8044682c3b3953f7a4c711fc89d54c
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2020-09-11 10:17:00 +0200

    Add timeout for stopping

--- nextcloud.sh
+++ nextcloud.sh
@@ -2,12 +2,14 @@
 
 set -euo pipefail
 
+cd $(dirname $0)
+
 case $1 in
 	start )
 		docker-compose up --build --abort-on-container-exit --force-recreate
 		;;
 	stop )
-		docker-compose down --volumes
+		docker-compose down --volumes --timeout 300
 		;;
 	cron )
 		docker-compose exec -u www-data -d nextcloud php cron.php
