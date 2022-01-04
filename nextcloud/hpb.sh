b28eebdfff556c9bc9dd6f1c65f038488c61d2f7
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2022-01-04 12:56:29 +0100

    Minor updates

--- nextcloud/hpb.sh
+++ nextcloud/hpb.sh
@@ -1,9 +1,12 @@
-#!/bin/sh
-set -eu
+#!/bin/bash
+
+set -euxo pipefail
+
+./occ check
 
 if ! ./occ app:getpath notify_push; then
 	./occ app:install notify_push
 	./occ app:enable notify_push
 fi
 
-exec `./occ app:getpath notify_push`/bin/`uname -m`/notify_push config/config.php
+exec `./occ app:getpath notify_push`/bin/`uname -m`/notify_push ./config/config.php
