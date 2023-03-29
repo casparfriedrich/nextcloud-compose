38d9f2b5ea4fbba6b66ef6362428d2dfad7519a6
Caspar Friedrich <c.s.w.friedrich@gmail.com>
2023-03-29 10:50:52 +0200

    Absolute paths

--- nextcloud/notify_push.sh
+++ nextcloud/notify_push.sh
@@ -2,11 +2,11 @@
 
 set -euxo pipefail
 
-./occ check
+/var/www/html/occ check
 
-if ! ./occ app:getpath notify_push; then
-	./occ app:install notify_push
-	./occ app:enable notify_push
+if ! /var/www/html/occ app:getpath notify_push; then
+	/var/www/html/occ app:install notify_push
+	/var/www/html/occ app:enable notify_push
 fi
 
-exec `./occ app:getpath notify_push`/bin/`uname -m`/notify_push ./config/config.php
+exec `/var/www/html/occ app:getpath notify_push`/bin/`uname -m`/notify_push ./config/config.php
--- nextcloud/previewgenerator.sh
+++ nextcloud/previewgenerator.sh
@@ -2,15 +2,15 @@
 
 set -euxo pipefail
 
-./occ check
+/var/www/html/occ check
 
-if ! ./occ app:getpath previewgenerator; then
-	./occ app:install previewgenerator
-	./occ app:enable previewgenerator
+if ! /var/www/html/occ app:getpath previewgenerator; then
+	/var/www/html/occ app:install previewgenerator
+	/var/www/html/occ app:enable previewgenerator
 fi
 
 while true
 do
-	./occ preview:pre-generate
+	/var/www/html/occ preview:pre-generate
 	sleep 1h
 done
